1bedee27696be37070a07e6ddfb6e4699e0f944f
fbernaudo22 <113998875+fbernaudo22@users.noreply.github.com>
2022-11-15 14:21:12 +0100

    Initial Commit

---
+++ SkinVisionT/SkinVisionTApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinVisionTApp.swift
+//  SkinVisionT
+//
+//  Created by Francesco Bernaudo on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct SkinVisionTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
